9d726fe77b903304d5758e847dbcd4e511f72102
HeartKush <andresparra0905@gmail.com>
2021-11-26 10:37:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Endpoints
+
+Get all posts: `GET /api/posts`
+
+Get a single post: `GET /api/posts/{id}`
+
+Create a new post: `POST /api/posts`
+
+Update a post: `PUT /api/posts/{id}`
+
+Delete a post: `DELETE /api/posts/{id}`
---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\Post;
+use App\Http\Controllers\PostsApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/posts', [PostsApiController::class, 'index']);
+Route::post('/posts', [PostsApiController::class, 'store']);
+Route::put('/posts/{post}', [PostsApiController::class, 'update']);
+Route::delete('/posts/{post}', [PostsApiController::class, 'destroy']);
